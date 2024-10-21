6942a19e1a4a7f9b141316a51ea0bb8a8c5659e7
strdinc <n.filatov@i-digit.ru>
2024-10-21 12:38:32 +0700

    anim 1

--- js/anim.js
+++ js/anim.js
@@ -1,19 +1,37 @@
 window.addEventListener('scroll', function() {
-    const scrollPosition = window.pageYOffset;
-    const anim = document.querySelectorAll('.scroll-anim-y');
+  const windowHeight = window.innerHeight;
+  const scrollPosition = window.pageYOffset;
 
-    anim.forEach(function(anim) {
-        const speed = 0.2; 
-        anim.style.transform = `translateY(${scrollPosition*speed}px)`;
-    });
+  const animY = document.querySelectorAll('.scroll-anim-y');
+  const animX = document.querySelectorAll('.scroll-anim-x');
+
+  animY.forEach(function(anim) {
+    const animTop = anim.getBoundingClientRect().top; // Позиция верхней грани элемента относительно окна
+
+    if (animTop < windowHeight / 2) {  // Если элемент достиг середины экрана
+      const speed = 0.2;
+      const translateY = (scrollPosition - anim.offsetTop + windowHeight / 2) * speed;
+      anim.style.transform = `translateY(${translateY}px)`;
+    }
+  });
+
+  animX.forEach(function(anim) {
+    const animTop = anim.getBoundingClientRect().top; // Позиция верхней грани элемента относительно окна
+
+    if (animTop < windowHeight / 2) {  // Если элемент достиг середины экрана
+      const speed = 0.12;
+      const translateX = (scrollPosition - anim.offsetTop + windowHeight / 2) * speed;
+      anim.style.transform = `translateX(${translateX}px)`;
+    }
+  });
 });
 
 window.addEventListener('scroll', function() {
-    const scrollPosition = window.pageYOffset;
-    const anim = document.querySelectorAll('.scroll-anim-x');
+  const scrollPosition = window.pageYOffset;
+  const anim = document.querySelectorAll('.scroll-anim-y-cross');
 
-    anim.forEach(function(anim) {
-        const speed = 0.12; 
-        anim.style.transform = `translateX(${scrollPosition*speed}px)`;
-    });
+  anim.forEach(function(anim) {
+    const speed = 0.2;
+    anim.style.transform = `translateY(${scrollPosition*speed}px)`;
+  });
 });
