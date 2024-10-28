aba10ba1777e9b30bfb4843186d504663d84f72f
strdinc <n.filatov@i-digit.ru>
2024-10-29 02:29:16 +0700

    123456789

--- js/toggleBut.js
+++ js/toggleBut.js
@@ -1,11 +1,17 @@
+let isToggled = false; // Следим за состоянием
+
 function toggleColorAndEnableSubmit(button) {
   // Изменение цвета кнопки
-  if (button.style.backgroundColor === "rgb(21, 233, 195)") {
-    button.style.backgroundColor = "transparent";
-    document.getElementById("subbut").disabled = true;
-  } else {
+  if (!isToggled) {
     button.style.backgroundColor = "#15E9C3";
     document.getElementById("subbut").disabled = false;
     document.getElementById("subbut").style.cursor = "pointer";
+    document.getElementById("subbut").style.backgroundColor = "#4CAF50"; // Цвет активной кнопки
+  } else {
+    button.style.backgroundColor = "#4CAF50";
+    document.getElementById("subbut").disabled = true;
+    document.getElementById("subbut").style.cursor = "not-allowed";
+    document.getElementById("subbut").style.backgroundColor = "#ccc"; // Цвет заблокированной кнопки
   }
+  isToggled = !isToggled; // Переключаем состояние
 }
--- submit.php
+++ submit.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "hackathon";
-$dbname = "participants";
+$username = "u2873467_default";
+$password = "RRatTB565cjRv7vK";
+$dbname = "u2873467_default";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -17,8 +17,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $institution = $conn->real_escape_string($_POST['institution']);
     $email = $conn->real_escape_string($_POST['email']);
     $phone = $conn->real_escape_string($_POST['phone']);
-    $sql = "INSERT INTO participants (fio, track, institution, email, phone)
-            VALUES ('$fio', '$track', '$institution', '$email', '$phone')";
+    
+    // Проверка поля team-name
+    $team_name = !empty($_POST['team-name']) ? $conn->real_escape_string($_POST['team-name']) : "no team";
+    
+    $sql = "INSERT INTO participants (fio, track, institution, email, phone, team_name)
+            VALUES ('$fio', '$track', '$institution', '$email', '$phone', '$team_name')";
     if ($conn->query($sql) === TRUE) {
         echo "Данные успешно отправлены!";
     } else {
